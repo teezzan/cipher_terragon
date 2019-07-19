32b5d217816d99e8336c4349a835616885d7284f
teehazzan <teehazzan@gmail.com>
2019-07-19 12:55:16 +0100

    improved cipher

--- cyphered.py
+++ cyphered.py
@@ -3,6 +3,9 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
 Ui_MainWindow, QtBaseClass = uic.loadUiType("cypherUI.ui")
+import base64
+
+
 
 lis= {'a':[3,3,2],'b':[2,3,2],'c':[1,2,3],'d':[1,2,1],'e':[1,1,1],'f':[3,1,1],'g':[3,1,2],'h':[3,3,1],'i':[3,2,2],'j':[3,2,3],'h':[3,3,1],'i':[3,2,2],'j':[3,2,3],'k':[2,2,2],'l':[2,2,1],'m':[2,1,1],'n':[2,3,1],'o':[3,1,3],'p':[1,1,2],'q':[3,3,3],'r':[3,2,1],'s':[1,1,3],'t':[2,3,3],'u':[1,2,2],'v':[1,3,1],'w':[1,3,2],'x':[2,2,3],'y':[1,3,3],'z':[2,1,3],' ':[2,1,2]}
 
@@ -30,7 +33,9 @@ class MyApp(QMainWindow):
         encoded=""
         for i in range(0,len(text),key):
             encoded+=self.encode_key(text[i:i+key])
-        return encoded
+        print("bse64:"+str(base64.b64encode(str.encode(encoded))))
+        te=str(base64.b64encode(str.encode(encoded)))
+        return te[2:len(te)-1]
 
     def encode_key(self,inp):
         cyp=""
@@ -40,12 +45,23 @@ class MyApp(QMainWindow):
                 cyp+=str(lis[j][i])
         for k in range(0,len(cyp),3):
             out+=str(list(lis.keys())[list(lis.values()).index([int(cyp[k]),int(cyp[k+1]),int(cyp[k+2])])])
+        #return base64.b64encode(str.encode(out))#out
         return out
         
     #print(list(lis.keys())[list(lis.values()).index([3,3,2])])
     
     def decode_input(self,text,key=5):
+        print("init",text)
+        te=str(text)
+        te=str(base64.b64decode(str.encode(te)))
+        print("")
+        print("te real:"+te)
+        te=str(te)
+        te=te[2:len(te)-1]
+        text=te
+        print("TXT:",text)
         decoded=""
+        
         for i in range(0,len(text),key):
             decoded+=self.decode_key(text[i:i+key])
         return decoded
